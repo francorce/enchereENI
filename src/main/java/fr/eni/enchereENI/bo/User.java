107564efd755fdd4c1018f11dc5c74d2555edfb6
Pierrew151 <70957565+Pierrew151@users.noreply.github.com>
2022-04-05 11:23:44 +0200

    ajout class user

---
+++ src/main/java/fr/eni/enchereENI/bo/User.java
@@ -0,0 +1,110 @@
+package fr.eni.enchereENI.bo;
+import java.util.Objects;
+
+public class User {
+	private int no_utilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String cp;
+	private String ville;
+	private String password;
+	private int credit;
+	private boolean isAdmin;
+	public int getNo_utilisateur() {
+		return no_utilisateur;
+	}
+	public void setNo_utilisateur(int no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	@Override
+	public String toString() {
+		return "User [no_utilisateur=" + no_utilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom=" + prenom
+				+ ", email=" + email + ", telephone=" + telephone + ", cp=" + cp + ", ville=" + ville + ", password="
+				+ password + ", credit=" + credit + ", isAdmin=" + isAdmin + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(cp, credit, email, isAdmin, no_utilisateur, nom, password, prenom, pseudo, telephone,
+				ville);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(cp, other.cp) && credit == other.credit && Objects.equals(email, other.email)
+				&& isAdmin == other.isAdmin && no_utilisateur == other.no_utilisateur && Objects.equals(nom, other.nom)
+				&& Objects.equals(password, other.password) && Objects.equals(prenom, other.prenom)
+				&& Objects.equals(pseudo, other.pseudo) && Objects.equals(telephone, other.telephone)
+				&& Objects.equals(ville, other.ville);
+	}
+	
+
+}
