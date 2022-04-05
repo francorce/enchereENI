dc7a02d1b01ffe9be1a21bd1f79228260fd02542
Pierrew151 <70957565+Pierrew151@users.noreply.github.com>
2022-04-05 11:33:36 +0200

    ajout userDao

---
+++ src/main/java/fr/eni/enchereENI/dao/UserDao.java
@@ -0,0 +1,41 @@
+package fr.eni.enchereENI.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import fr.eni.enchereENI.bo.User;
+
+public  class UserDao implements Dao<User>{
+
+	@Override
+	public Optional<User> get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User t, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
