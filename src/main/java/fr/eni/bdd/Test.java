3b10b652b724ea7121323d910edf80323b80fa60
Francorce <69760008+francorce@users.noreply.github.com>
2022-04-05 10:18:01 +0200

    connection provider

---
+++ src/main/java/fr/eni/bdd/Test.java
@@ -0,0 +1,24 @@
+package fr.eni.bdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+//		
+//		try {
+//			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ekip_eni","root","");
+//			System.out.println("connection réussie");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+		
+		
+	}
+
+}
